79ab662f012fe694ffd763f9213e00f855f67b79
George Tang <georgecctang@gmail.com>
2020-09-23 18:52:13 +0000

    updated client.js

--- client.js
+++ client.js
@@ -5,6 +5,9 @@ const net = require('net');
  * Establishes connection with the game server
  */
 const connect = function() {
+
+  console.log('Connecting ...');
+  
   const conn = net.createConnection({ 
     host: 'localhost',
     port: 50541
@@ -15,11 +18,11 @@ const connect = function() {
   conn.on('connect', () => {
     console.log('Successfully connected to game server!');
     conn.write("Name: GEO");
-  })
+  });
 
   // log messsage from server
   conn.on('data', (data) =>{
-    console.log(data)
+    console.log(data);
   });
 
 
--- play.js
+++ play.js
@@ -1,9 +1,26 @@
 
 const { connect } = require('./client');
 
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    console.log("Disconnecting from game server...");
+    process.exit();  
+  }
+}
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+
+  return stdin;
+}
+
 // const stdin = process.stdin;
 // stdin.setRawMode(true);
 // stdin.setEncoding('utf8');
 
-console.log('Connecting ...');
 connect();
+setupInput();
