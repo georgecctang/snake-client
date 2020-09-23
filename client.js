b45580c448ab9510f497e10f20a689caea47a640
George Tang <georgecctang@gmail.com>
2020-09-23 18:32:46 +0000

    created client.js file, added conn listeners

---
+++ client.js
@@ -0,0 +1,29 @@
+const { read } = require('fs');
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: 'localhost',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server!');
+    conn.write("Name: GEO");
+  })
+
+  // log messsage from server
+  conn.on('data', (data) =>{
+    console.log(data)
+  });
+
+
+  return conn;
+}
+
+module.exports = { connect };
---
+++ play.js
@@ -0,0 +1,9 @@
+
+const { connect } = require('./client');
+
+// const stdin = process.stdin;
+// stdin.setRawMode(true);
+// stdin.setEncoding('utf8');
+
+console.log('Connecting ...');
+connect();
